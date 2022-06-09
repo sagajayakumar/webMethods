5c038d969c96e5ee27914b5aa98d5d2b0b3dbbba
sagajayakumar <jayakumar1994@gmail.com>
2022-06-09 17:07:41 -0500

    Add files via upload

---
+++ Acme/code/source/acme/Purchageorder/work.java
@@ -0,0 +1,44 @@
+package acme.Purchageorder;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class work
+
+{
+	// ---( internal utility methods )---
+
+	final static work _instance = new work();
+
+	static work _newInstance() { return new work(); }
+
+	static work _cast(Object o) { return (work)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void endsWith (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(endsWith)>> ---
+		// @sigtype java 3.5
+		IDataCursor cursor = pipeline.getCursor();
+		String string = IDataUtil.getString(cursor, "string");
+		String suffix = IDataUtil.getString(cursor, "suffix");
+		String value = string.endsWith(suffix) ? "true" : "false";
+		IDataUtil.put(cursor, "value", value);
+		cursor.destroy();
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ Acme/ns/acme/Purchageorder/work/endsWith/java.frag
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">endsWith</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">SURhdGFDdXJzb3IgY3Vyc29yID0gcGlwZWxpbmUuZ2V0Q3Vyc29yKCk7DQpTdHJpbmcgc3RyaW5n
+ID0gSURhdGFVdGlsLmdldFN0cmluZyhjdXJzb3IsICJzdHJpbmciKTsNClN0cmluZyBzdWZmaXgg
+PSBJRGF0YVV0aWwuZ2V0U3RyaW5nKGN1cnNvciwgInN1ZmZpeCIpOw0KU3RyaW5nIHZhbHVlID0g
+c3RyaW5nLmVuZHNXaXRoKHN1ZmZpeCkgPyAidHJ1ZSIgOiAiZmFsc2UiOw0KSURhdGFVdGlsLnB1
+dChjdXJzb3IsICJ2YWx1ZSIsIHZhbHVlKTsNCmN1cnNvci5kZXN0cm95KCk7DQo=</value>
+</Values>
